bc87e8f1247d53aa25dc7aa44cef17016b35ec25
Viktor Borisov <viko@vikobg.com>
2019-03-10 20:41:30 +0200

    fixing readstream solution

--- nodejs-json-to-csv/read-stream.js
+++ nodejs-json-to-csv/read-stream.js
@@ -1,14 +1,18 @@
 const csvjson = require('csvjson');
 const fs = require('fs');
+
 const stream = require('stream');
+const JSONStream = require('jsonstream');
 
 const createReadStream = fs.createReadStream;
 const createWriteStream = fs.createWriteStream;
 
+let line = 0;
 const transformJsonToCSV = new stream.Transform({
     transform: function transformer(chunk, encoding, callback) {
+        line++;
         callback(false, csvjson.toCSV(chunk, {
-            headers: 'key'
+            headers: line > 1 ? 'none' : 'key'
         }));
     },
     readableObjectMode: true,
@@ -16,5 +20,6 @@ const transformJsonToCSV = new stream.Transform({
 });
 
 createReadStream('./test-data.json', 'utf-8')
+    .pipe(JSONStream.parse('*'))
     .pipe(transformJsonToCSV)
     .pipe(createWriteStream('./test-data-output-stream.csv'));
